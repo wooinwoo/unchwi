ca7c6d19ca31e8de1549b77f21d502ac1eaa7c01
wooinwoo <wooin1001@gmail.com>
2023-04-16 18:22:04 +0900

    add : 메인페이지 이름 변경

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import MainBanner from "../components/MainBanner";
+import PostContainer from "../components/post/PostContainer";
 
 function main() {
   return (
     <Wrap>
       <MainBanner />
+      <PostContainer />
     </Wrap>
   );
 }
