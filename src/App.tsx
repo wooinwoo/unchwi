12c2614514e35d394d2e61ab52ad716d97003f4a
wooinwoo <wooin1001@gmail.com>
2023-04-22 19:01:50 +0900

    add : 라우팅 추가 sign In, sign Out

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Main from "./pages/Main";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 import "./App.css";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
         </Routes>
       </Layout>
     </BrowserRouter>
