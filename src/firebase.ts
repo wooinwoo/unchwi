ac231193ef2506e988d6390206140f437da6d366
wooinwoo <wooin1001@gmail.com>
2023-04-23 16:49:58 +0900

    update: import 방식 변경

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,19 +5,30 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+
+const {
+  VITE_FB_API_KEY,
+  VITE_FB_AUTH_DOMAIN,
+  VITE_FB_PROJECT_ID,
+  VITE_FB_STORAGE_BUCKET,
+  VITE_FB_MESSAGING_SENDER_ID,
+  VITE_FB_API_ID,
+} = import.meta.env;
+
 //
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FB_API_KEY,
-  authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
-  projectId: import.meta.env.FB_PROJECT_ID,
-  storageBucket: import.meta.env.FB_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
-  appId: import.meta.env.FB_API_ID,
+  apiKey: VITE_FB_API_KEY,
+  authDomain: VITE_FB_AUTH_DOMAIN,
+  projectId: VITE_FB_PROJECT_ID,
+  storageBucket: VITE_FB_STORAGE_BUCKET,
+  messagingSenderId: VITE_FB_MESSAGING_SENDER_ID,
+  appId: VITE_FB_API_ID,
 };
 //
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
 const firebaseAuth = getAuth(firebaseApp);
+console.log(firebaseConfig);
 //
 // 꼭 이렇게 해야하는 건 아니니까 편한대로 해당 스크립트에서 import해서 사용해도 된다
 export {
